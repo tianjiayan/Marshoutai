5050a64f295252cd68a833e3dd9767b0bedc8d2a
YangMr <759168442@qq.com>
2022-06-25 15:17:10 +0800

    feat(git): feat(git): add commitizen

--- .cz-config.js
+++ .cz-config.js
@@ -7,7 +7,7 @@ module.exports = {
     { value: 'style', name: 'style: 代码格式(不影响代码运⾏的变动)' },
     {
       value: 'refactor',
-      name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
+      name: 'refactor: 重构(既不是增加feature,也不是修复bug)'
     },
     { value: 'perf', name: 'perf: 性能优化' },
     { value: 'test', name: 'test: 增加测试' },
@@ -15,6 +15,7 @@ module.exports = {
     { value: 'revert', name: 'revert: 回退' },
     { value: 'build', name: 'build: 打包' }
   ],
+
   // 消息步骤
   messages: {
     type: '请选择提交类型:',
@@ -23,9 +24,5 @@ module.exports = {
     body: '请输⼊详细描述(可选):',
     footer: '请输⼊要关闭的issue(可选):',
     confirmCommit: '确认使⽤以上信息提交？(y/n/e/h)'
-  },
-  // 跳过问题
-  skipQuestions: ['body', 'footer'],
-  // subject⽂字⻓度默认是72
-  subjectLimit: 72
+  }
 }
