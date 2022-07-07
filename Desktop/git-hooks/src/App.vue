8cce91751bc55957411b5f390f3ef38cb84fa687
Tian <11132373+tianjiayan@user.noreply.gitee.com>
2022-07-07 19:36:17 +0800

    feat: login

---
+++ Desktop/git-hooks/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+<style lang="scss">
+html,
+body,
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ Desktop/git-hooks/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    <p>你好</p>
+  </div>
+</template>
+
+<script>
+</script>
